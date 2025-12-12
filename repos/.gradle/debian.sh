97bd14abc8e75f6c3237a7a21f4a7df3e615b9e1
gebz97 <156537226+gebz97@users.noreply.github.com>
2025-12-12 12:35:03 +0200

    Create debian.sh

---
+++ repos/.gradle/debian.sh
@@ -0,0 +1,5 @@
+deb http://nexus.gebz.local:8080/repository/debian-trixie/ trixie main contrib non-free non-free-firmware
+
+deb http://nexus.gebz.local:8080/repository/debian-trixie-security/ trixie-security main contrib non-free non-free-firmware
+
+deb http://nexus.gebz.local:8080/repository/debian-trixie-updates/ trixie-updates  main contrib non-free non-free-firmware
